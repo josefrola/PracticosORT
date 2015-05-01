c67b54e8bf211f8c65e4e6b8a96e9fa71ccb5522
jose <jose@jose-X502CA>
2015-04-30 22:27:02 -0300

    ejercici del practico 3

--- Practico2/js/funciones.js
+++ Practico2/js/funciones.js
@@ -5,10 +5,10 @@ $(document).ready(LoadFunction);
 
 /*------------------------------variables glovales ---------------------------------*/
 
-var contador=0;
-var acumulador1=0;
-var acumulador2=0;
-var acumulador3=0;
+var contador = 0;
+var acumulador1 = 0;
+var acumulador2 = 0;
+var acumulador3 = 0;
 
 /*-------------------------------------fin de las variables glovales --------------*/
 
@@ -26,12 +26,13 @@ function LoadFunction() {
     $('#btnCalcularEjer11').click(Ejercicio11);
     $('#btnEje12Calcular').click(Ejericio12);
     $('#btnCalcularEje13').click(Ejericio13);
-    
+    $('#btnTemperatura').click(Ejercicio14);
+
 }
 
 
 function MostrarData() {
-    
+
     var number = parseInt($('#txtName').val());
 
     if (number >= 0) {
@@ -42,181 +43,208 @@ function MostrarData() {
 
 }
 
-function CheckNumber(){
-    
-    var number=parseInt($('#txtName').val());
-    
-    if(number > 10){
+function CheckNumber() {
+
+    var number = parseInt($('#txtName').val());
+
+    if (number > 10) {
         alert('Número mayor que 10');
     }
 }
 
-function FunctionEjer3(){
-    var number=parseInt($('#txtNum').val());
-    
-    if(number > 20){
+function FunctionEjer3() {
+    var number = parseInt($('#txtNum').val());
+
+    if (number > 20) {
         alert('El Número es mayor que 20');
-    }else if(number <= 20){
-       alert('El numero es menor o igual a 20'); 
+    } else if (number <= 20) {
+        alert('El numero es menor o igual a 20');
     }
 }
 
-function FunctionEjer4(){
-    var number=parseInt($('#txtNumber4').val());
-    var value=0;
-    var message="";
-    if(number < 0){
-        value=number * -1;
-        message="El Número es :" + value; 
-    }    
-    $('#divShowData').html(message);   
+function FunctionEjer4() {
+    var number = parseInt($('#txtNumber4').val());
+    var value = 0;
+    var message = "";
+    if (number < 0) {
+        value = number * -1;
+        message = "El Número es :" + value;
+    }
+    $('#divShowData').html(message);
 }
 
-function Ejericio5(){
-    var numbr_1=Math.abs($('#txtNumber1').val());
-    var number_2=Math.abs($('#txtNumber2').val());
-    var number_3=Math.abs($('#txtNumber3').val());
-    var number_4=Math.abs($('#txtNumber4').val());
-    var number_5=Math.abs($('#txtNumber5').val());
-    var number_6=Math.abs($('#txtNumber6').val());
-    
-    var total=0;
-    total=numbr_1 + number_2 + number_3 + number_4 + number_5 + number_6;
+function Ejericio5() {
+    var numbr_1 = Math.abs($('#txtNumber1').val());
+    var number_2 = Math.abs($('#txtNumber2').val());
+    var number_3 = Math.abs($('#txtNumber3').val());
+    var number_4 = Math.abs($('#txtNumber4').val());
+    var number_5 = Math.abs($('#txtNumber5').val());
+    var number_6 = Math.abs($('#txtNumber6').val());
+
+    var total = 0;
+    total = numbr_1 + number_2 + number_3 + number_4 + number_5 + number_6;
     $('#divResult').html(total);
-    
+
 }
 
-function Ejericio6(){
-    var number=parseInt($('#txtNumberEj6'));
-    
-    if((number > 10) && (number < 20)){
+function Ejericio6() {
+    var number = parseInt($('#txtNumberEj6'));
+
+    if ((number > 10) && (number < 20)) {
         alert('Es mayor que 10 y menor que 20');
-    }else{
+    } else {
         alert('No es mayor que 10 ni menor a 20');
     }
 }
 
-function Ejericio7(){
-    var numero=(parseInt($('#txtNumberEj7').val()));
-    var total=numero % 7;
-    var total1=numero % 3;
-    
-    if((total===0)&&(total1===0)){
+function Ejericio7() {
+    var numero = (parseInt($('#txtNumberEj7').val()));
+    var total = numero % 7;
+    var total1 = numero % 3;
+
+    if ((total === 0) && (total1 === 0)) {
         alert('Los números ingresados son multiplos de 7 y 3');
-    }else{
+    } else {
         alert('Los Números ingresados nos son multiplos de 7 y 3');
     }
 }
 
-function Ejercicio8(){
-    var numero=(parseInt($('#txtNumberEj8').val()));
-    var negativo= -20;
-    var positivo= 20;
-   
-    if((numero < negativo)||(numero > positivo)){
+function Ejercicio8() {
+    var numero = (parseInt($('#txtNumberEj8').val()));
+    var negativo = -20;
+    var positivo = 20;
+
+    if ((numero < negativo) || (numero > positivo)) {
         alert('Cumple');
-    }else{
+    } else {
         alert('No Cumple');
     }
 }
 
-function Ejericio9(){
-    var numero=(parseInt($('#txtNumber_1Ejp9').val()));
-    var total=0;
-    total=numero*5;
+function Ejericio9() {
+    var numero = (parseInt($('#txtNumber_1Ejp9').val()));
+    var total = 0;
+    total = numero * 5;
     console.log(total);
-    
+
 }
 
-function Ejericio10(){
-    var numero=(parseInt($('#txtnumero').val()));
-    
-    if(numero > 30){
+function Ejericio10() {
+    var numero = (parseInt($('#txtnumero').val()));
+
+    if (numero > 30) {
         alert('Número es mayor que 30');
-    }else if(numero < 10){
+    } else if (numero < 10) {
         alert('Número menor que 10');
-    }else{
+    } else {
         alert('Numero esta entre 10 y 30');
     }
 }
 
-function Ejercicio11(){
-    var minAzucar=1;
-    var minHarina=100;
-    var minAceite=1;
-    var minAgua=1;
-    
-    var Azucar=(parseInt($('#txtazucar').val()));
-    var Harina=(parseInt($('#txtharina').val()));
-    var Aceite=(parseInt($('#txtaceite').val()));
-    var Agua=(parseInt($('#txtagua').val()));
-    
-    var canMinima=Number.MAX_VALUE;
-    
-    var gAzucar=Azucar / minAzucar;
-    var gHarina=Harina / minHarina;
-    var gAceite=Aceite / minAceite;
-    var gAgua=Agua / minAgua;
-    
-    if(gAzucar < canMinima)canMinima=gAzucar;
-    if(gHarina < canMinima)canMinima=gHarina;
-    if(gAceite < canMinima)canMinima=gAceite;
-    if(gAgua < canMinima)canMinima=gAgua;
-    
-    $('#mensaje').html('La cantidad minima es :'+ canMinima);
+function Ejercicio11() {
+    var minAzucar = 1;
+    var minHarina = 100;
+    var minAceite = 1;
+    var minAgua = 1;
+
+    var Azucar = (parseInt($('#txtazucar').val()));
+    var Harina = (parseInt($('#txtharina').val()));
+    var Aceite = (parseInt($('#txtaceite').val()));
+    var Agua = (parseInt($('#txtagua').val()));
+
+    var canMinima = Number.MAX_VALUE;
+
+    var gAzucar = Azucar / minAzucar;
+    var gHarina = Harina / minHarina;
+    var gAceite = Aceite / minAceite;
+    var gAgua = Agua / minAgua;
+
+    if (gAzucar < canMinima)
+        canMinima = gAzucar;
+    if (gHarina < canMinima)
+        canMinima = gHarina;
+    if (gAceite < canMinima)
+        canMinima = gAceite;
+    if (gAgua < canMinima)
+        canMinima = gAgua;
+
+    $('#mensaje').html('La cantidad minima es :' + canMinima);
 }
 
-function Ejericio12(){
-    var valorcasa =parseInt($('#txtprecioCasa').val());
-    var ingreso=parseInt($('#txtingresocomprador').val());
-    var porsentajePaga=0;
-    var restoPagar=0;
-    var cuotas;   
-    var messageporsentaje="";
-    var message="";
-    
-    if(ingreso < 20000){
-        porsentajePaga=valorcasa * 0.15;
-        restoPagar=valorcasa - porsentajePaga;
-        cuotas=restoPagar / 2;
-        messageporsentaje="Debe pagar el 15% del valor de l casa que es:" + porsentajePaga ;
-        message="El resto a pagar son dos cuotas de:" + cuotas;
-    }else if(ingreso >= 20000){
-        var mesesAno=12;
-        var totalmeses=0;
-       porsentajePaga= valorcasa * 0.30;
-       restoPagar=valorcasa - porsentajePaga;
-       totalmeses=mesesAno * 7;
-       cuotas=restoPagar / totalmeses;
-       messageporsentaje="Debe pagar el 30% del valor de l casa que es:" + porsentajePaga;
-       message="El resto a pagar son "+ totalmeses +" cuotas de:" + cuotas;
+function Ejericio12() {
+    var valorcasa = parseInt($('#txtprecioCasa').val());
+    var ingreso = parseInt($('#txtingresocomprador').val());
+    var porsentajePaga = 0;
+    var restoPagar = 0;
+    var cuotas;
+    var messageporsentaje = "";
+    var message = "";
+
+    if (ingreso < 20000) {
+        porsentajePaga = valorcasa * 0.15;
+        restoPagar = valorcasa - porsentajePaga;
+        cuotas = restoPagar / 2;
+        messageporsentaje = "Debe pagar el 15% del valor de l casa que es:" + porsentajePaga;
+        message = "El resto a pagar son dos cuotas de:" + cuotas;
+    } else if (ingreso >= 20000) {
+        var mesesAno = 12;
+        var totalmeses = 0;
+        porsentajePaga = valorcasa * 0.30;
+        restoPagar = valorcasa - porsentajePaga;
+        totalmeses = mesesAno * 7;
+        cuotas = restoPagar / totalmeses;
+        messageporsentaje = "Debe pagar el 30% del valor de l casa que es:" + porsentajePaga;
+        message = "El resto a pagar son " + totalmeses + " cuotas de:" + cuotas;
     }
-    
+
     $('#porsentajepago').html(messageporsentaje);
     $('#coutasPagar').html(message);
 }
 
-function Ejericio13(){
-    var tipopago=$('#tipodepago').val();
-    var montoPagar=parseFloat($('#montoPagar').val());
-    var descuento=0.04;
-    var total=0;
-    var montoInferior=10000;
-    var message="";
-   
-    if((tipopago=='2' || tipopago=='3') &&(montoPagar < montoInferior)){
-        var descuento=montoPagar * descuento;
-        total=montoPagar - descuento;
-        message="<div> Total:"+ montoPagar +"<br>Descuento:"+ descuento +"<br>Total a Pagar:"+ total +"</div>"
-    }else{
-       message="Total:"+  montoPagar;
+function Ejericio13() {
+    var tipopago = $('#tipodepago').val();
+    var montoPagar = parseFloat($('#montoPagar').val());
+    var descuento = 0.04;
+    var total = 0;
+    var montoInferior = 10000;
+    var message = "";
+
+    if ((tipopago == '2' || tipopago == '3') && (montoPagar < montoInferior)) {
+        var descuento = montoPagar * descuento;
+        total = montoPagar - descuento;
+        message = "<div> Total:" + montoPagar + "<br>Descuento:" + descuento + "<br>Total a Pagar:" + total + "</div>"
+    } else {
+        message = "Total:" + montoPagar;
     }
-    
+
     $('#totalPagar').html(message);
 }
 
-function Ejercicio14(){
-    //var temperatura=parseInt();
+function Ejercicio14() {
+    var temperatura = parseInt($('#temperatura').val());
+    var dias = $('#diasSemana').val();
+    var message = "";
+    if ((temperatura < 10)) {
+        if (dias == "Lunes" || dias == "Martes" || dias == "Miercoles" || dias == "Jueves" || dias == "Viernes" || dias == "Sabado") {
+            message = "Levantarse abrigarse mucho. Ir al trabajo";
+        } else {
+            message = "Levantarse abrigarse que esta frio, hoy no trabaja";
+        }
+    } else if (temperatura > 20) {
+        if (dias == "Lunes" || dias == "Martes" || dias == "Miercoles" || dias == "Jueves" || dias == "Viernes" || dias == "Sabado") {
+            message = "Levantarse ponerse ropa comoda . Ir al trabajo";
+        } else {
+            message = "Levantarse ponerse ropa comoda , hoy no trabaja";
+        }
+    } else if (temperatura > 10 && temperatura < 20) {
+        if (dias == "Lunes" || dias == "Martes" || dias == "Miercoles" || dias == "Jueves" || dias == "Viernes" || dias == "Sabado") {
+            message = "Levantarse ponerse abrigo intermedio . Ir al trabajo";
+        } else {
+            message = "Levantarse ponerse abrigo intermedio , hoy no trabaja";
+        }
+    }
+    
+    $('#message').html(message);
 }
 
 
--- Practico3/js/funciones.js
+++ Practico3/js/funciones.js
@@ -7,14 +7,27 @@ function CargarTodo() {
     $('#btnejecutar').click(Ejercicio2);
     $('#btnejecutar1').click(Ejercicio2While);
     $('#btnmostrardo').click(Ejercicio2do);
-    
+
     $('#btnejercicio3').click(Ejercicio3);
     $('#btnmostrarwhile').click(Ejercicio3While);
     $('#btnmostrardo3').click(Ejercicio3do);
-    
+
     $('#btnMostrarEje4_1').click(Ejercicio4for);
     $('#btnMostrarEje4_2').click(Ejericicio4While);
     $('#btnMostrarEje4_3').click(Ejercicio4do);
+
+    $('#btnejercicio5for').click(Ejercicio5for);
+    $('#btnejercicio5while').click(Ejericiowhile);
+    $('#btnejercicio5do').click(Ejercicio5do);
+
+
+    $('#btnEjericio6For').click(Ejericio6For);
+    $('#btnEjericio6while').click(Ejericio6While);
+    $('#btnEjericio6do').click(Ejercicio6do);
+
+    $('#btnForEjericiocio7').click(Ejercicio7for);
+    $('#btnwhileEjericiocio7').click(Ejercicio7While);
+    $('#btndoEjericiocio7').click(Ejjericicio7Do);
 }
 
 /*----------------------------ejercicio 2 ------------------------------*/
@@ -32,20 +45,20 @@ function Ejercicio2While() {
     var i = 0;
     while (i <= 100) {
         number += "Numeros :" + i + "<br>";
-        
+
         i++;
     }
     $('#numero_1').html(number);
 }
 
-function Ejercicio2do(){
-    var numero="";
-    var i =0;
-    do{
+function Ejercicio2do() {
+    var numero = "";
+    var i = 0;
+    do {
         numero += "Numero: " + i + "<br>";
         i++;
-    }while(i <= 100);
-    
+    } while (i <= 100);
+
     $('#numero_2').html(numero);
 }
 
@@ -62,24 +75,24 @@ function Ejercicio3() {
     $('#mostrarNumero').html(numero);
 }
 
-function Ejercicio3While(){
-    var numero="";
-    var i=-100;
-    while(i <= 10){
-        numero +="Numero: " + i + "<br>";
+function Ejercicio3While() {
+    var numero = "";
+    var i = -100;
+    while (i <= 10) {
+        numero += "Numero: " + i + "<br>";
         i++;
     }
     $('#mostrarNumero2').html(numero);
 }
 
-function Ejercicio3do(){
-    var numero="";
-    var i=-100;
-    do{
-       numero += "Numero : " + i + "<br>";
-       i++; 
-    }while(i <= 10); 
-    
+function Ejercicio3do() {
+    var numero = "";
+    var i = -100;
+    do {
+        numero += "Numero : " + i + "<br>";
+        i++;
+    } while (i <= 10);
+
     $('#pmostarNumero3do').html(numero);
 }
 
@@ -87,37 +100,190 @@ function Ejercicio3do(){
 
 /*---------------------------Ejercicio 4------------------------------------*/
 
-function Ejercicio4for(){
-    var numero="";
-    for(var i=40; i >= 10; i--){
+function Ejercicio4for() {
+    var numero = "";
+    for (var i = 40; i >= 10; i--) {
         numero += "Numero: " + i + "<br>";
     }
     $('#pMostrarEje3_for').html(numero);
 }
 
-function Ejericicio4While(){
-    var numero="";
-    var i= 40;
-    while(i >= 10){
-       numero += "Numero: " + i + "<br>"; 
-       i--;
+function Ejericicio4While() {
+    var numero = "";
+    var i = 40;
+    while (i >= 10) {
+        numero += "Numero: " + i + "<br>";
+        i--;
     }
     $('#pMostrarEje3_for').html(numero);
-    
+
 }
 
-function Ejercicio4do(){
-    var numero="";
-    var i=40;
-    do{
-        numero +="Numero: " + i + "<br>";
+function Ejercicio4do() {
+    var numero = "";
+    var i = 40;
+    do {
+        numero += "Numero: " + i + "<br>";
         i--;
-    }while(i >= 10);
-    
+    } while (i >= 10);
+
     $('#pMostrarEje4_do').html(numero);
 }
 
 /*---------------------------Fin Ejercicio 4------------------------------------*/
+
+/*----------------------------Ejercicio 5-----------------------------------------*/
+function Ejercicio5for() {
+
+    var numero = "";
+    for (var i = 20; i >= -30; i--) {
+        numero += "Numero:" + i + "<br>";
+
+    }
+    $('#divforEjercicio5').html(numero);
+}
+
+function Ejericiowhile() {
+    var i = 20;
+    var numero = "";
+    while (i >= -30) {
+        numero += "Numero:" + i + "<br>";
+
+        i--;
+    }
+    $('#divwhileEjercicio5').html(numero);
+}
+;
+
+function Ejercicio5do() {
+    var i = 20;
+    var numero = "";
+    do {
+
+        numero += "Numero: " + i + "<br>";
+        i--;
+
+    } while (i >= -30);
+
+
+    $('#divdoEjercicio5').html(numero);
+}
+
+/*----------------------------Fin Ejercicio 5-----------------------------------*/
+/*---------------------------- Ejercicio 6-------------------------------------*/
+
+function Ejericio6For() {
+    var numeroMultiplo = "";
+    var resultado = 0;
+
+    for (var i = 1; i <= 450; i++) {
+
+        resultado = parseInt(i % 5);
+
+        if (resultado === 0) {
+            numeroMultiplo += "Numero:" + i + "<br>";
+        }
+
+    }
+
+    $('#numerosMultiples').html(numeroMultiplo);
+
+}
+
+function Ejericio6While() {
+    var NumeroMultiplo = "";
+    var resultado = 0;
+    var i = 1;
+
+    while (i <= 450) {
+        resultado = parseInt(i % 5);
+
+        if (resultado === 0) {
+            NumeroMultiplo += "Numero:" + i + "<br>";
+        }
+
+        i++;
+    }
+    $('#numeroMultiplowhile').html(NumeroMultiplo);
+}
+
+function Ejercicio6do() {
+    var NumeroMultiple = "";
+    var resultado = 0;
+    var i = 1;
+
+    do {
+        resultado = parseInt(i % 5);
+
+        if (resultado === 0) {
+            NumeroMultiple += "Numero:" + i + "<br>";
+        }
+
+        i++;
+
+    } while (i <= 450);
+
+    $('#numeroMultiplodo').html(NumeroMultiple);
+}
+
+/*----------------------------Fin Ejercicio 6-------------------------------------*/
+
+/*---------------------------- Ejercicio 7----------------------------------------*/
+function Ejercicio7for() {
+    var NumeroMultiplo = "";
+    var resultado = 0;
+
+    for (var i = -33; i <= 230; i++) {
+
+        resultado = parseInt(i % 4);
+
+        if (resultado === 0) {
+            NumeroMultiplo += "Numero:" + i + "<br>";
+        }
+    }
+
+    $('#divNumerosFor').html(NumeroMultiplo);
+}
+
+function Ejercicio7While() {
+    var NumeroMultiple = "";
+    var resultado = 0;
+    var i = -33;
+
+    while (i <= 230) {
+
+        resultado = parseInt(i % 4);
+        if (resultado === 0) {
+           NumeroMultiple +="Numero:" + i + "<br>"; 
+        }
+        
+        i++;
+    }
+    
+    $('#divNumeroswhile').html(NumeroMultiple);
+}
+
+function Ejjericicio7Do(){
+   var NumeroMultiple = "";
+    var resultado = 0;
+    var i = -33;
+    
+    do{
+        resultado=parseInt(i % 4);
+        
+        if(resultado===0){
+            NumeroMultiple +="Numero:" + i +"<br>";
+        }
+        i++;
+        
+    }while(i <= 230);
+    
+    $('#divNumerosdo').html(NumeroMultiple);
+}
+
+/*---------------------------- Ejercicio 7----------------------------------------*/
+
+
 
 
 
